10e7b9ba7ffb32c5bea67932b510f085fd5cdd41
diegopluna <dpl@cesar.school>
2023-04-10 15:53:37 -0300

    Added styled Login Screen to React, fixed Post Poll view in Django

--- when2poll/polls/views.py
+++ when2poll/polls/views.py
@@ -3,11 +3,10 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from polls.api.serializers import AvailabilityPollSerializer
-from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
-@csrf_exempt
+
 class AvailabilityPollView(APIView):
     def post(self, request):
         serializer = AvailabilityPollSerializer(data=request.data)
