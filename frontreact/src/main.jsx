dbfebfe1c2b3b39f80b567a74446c78444fc557e
diegopluna <dpl@cesar.school>
2023-04-10 21:54:47 -0300

    Created account creation screen in React

--- frontreact/src/main.jsx
+++ frontreact/src/main.jsx
@@ -3,31 +3,37 @@ import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
+import RegisterPage from './pages/RegisterPage'
 import Header from './components/Header'
 import Protected from './utils/Protected'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
+
 import App from './App'
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Header/>,
-    children: [
-      {
-        path: "/",
-        element: <Protected>
-          <HomePage />
-        </Protected>
-      },
-      {
-        path: "/login",
-        element: <LoginPage />
-      }
-    ],
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Header/>,
+//     children: [
+//       {
+//         path: "/",
+//         element: <Protected>
+//           <HomePage />
+//         </Protected>
+//       },
+//       {
+//         path: "/login",
+//         element: <LoginPage />
+//       }
+//     ],
+//   },
+// ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <RegisterPage />
+    {/* <RouterProvider router={router} /> */}
   </React.StrictMode>,
 )
--- frontreact/src/pages/LoginPage.jsx
+++ frontreact/src/pages/LoginPage.jsx
@@ -2,14 +2,71 @@ import React from 'react'
 
 const LoginPage = () => {
   return (
-    <div>
-        <form>
-            <input type="email" name="email" placeholder="Enter email" />
-            <input type="password" name="password" placeholder="Enter Password" />
-            <input type="submit" />
+    <div className='d-flex min-vh-100 min-vw-100 justify-content-center align-items-center' style={styles.body}>
+      <div  style={styles.login} >
+        <p className='h1 text-center' style={styles.title} >When2Poll</p>
+        <form className='needs-validation' style={styles.form} >
+            <div className='form-group was-validated' style={styles.formGroup}>
+              <input className='form-control' type="email" id="email" placeholder="Email" required/>
+              <div className='invalid-feedback'>
+                Insira seu email!
+              </div>
+            </div>
+            <div className='form-group was-validated' style={styles.formGroup}>
+              <input className='form-control' type="password" id="password" placeholder="Senha" required/>
+              <div className='invalid-feedback'>
+                Insira sua senha!
+              </div>
+            </div>
+            <div className='form-group form-check' style={styles.formGroup}>
+            <input type="checkbox" class="form-check-input" id="rememberCheck"/>
+            <label style={styles.remember} className="form-check-label" htmlFor="rememberCheck">Lembrar de mim</label>
+            </div>
+            <button style={styles.button} type="submit" className="btn btn-success w-100">Entrar</button>
         </form>
+      </div>
     </div>
+    
   )
 }
 
+const styles = {
+  body: {
+    backgroundColor: '#222831'
+  },
+  login: {
+    width: "360px",
+    height: "min-content",
+    padding: "20px",
+    borderRadius: "12px",
+    backgroundColor: '#393E46'
+  },
+  title: {
+    fontSize: "36px",
+    marginBottom: "25px",
+    color:'#EEEEEE'
+  },
+  form: {
+    fontSize: "20px"
+  },
+  formGroup: {
+    marginBottom: "12px"
+  },
+  button: {
+    backgroundColor: "#00ADB5"
+  },
+  remember: {
+    color: '#EEEEEE'
+  }
+}
+
+// const bgStyle = {
+//   backgroundColor: '#222831'
+// }
+// const formStyle = {
+//   backgroundColor: '#393E46'
+// }
+// const title = {
+//   color: '#EEEEEE'
+// }
 export default LoginPage
