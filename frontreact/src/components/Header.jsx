9461ae95ed80df32b09ccb96f50d74d2188482a5
diegopluna <dpl@cesar.school>
2023-04-12 18:25:30 -0300

    Implemented routing and responsive navigation bar

--- frontreact/src/components/Header.jsx
+++ frontreact/src/components/Header.jsx
@@ -1,15 +1,49 @@
-import React, {useContext} from 'react'
+import React from 'react'
+import Nav from 'react-bootstrap/Nav'
 import { Link, Outlet } from 'react-router-dom'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faHome} from '@fortawesome/free-solid-svg-icons'
 
-const Header = () => {
+const tabs = [{
+  route: "/",
+  icon: faHome,
+  label: "Início"
+}]
+
+const Header = (props) => {
   return (
     <>
-        <div>
-            <Link to="/">Home</Link>
-            <span> | </span>
-            <Link to="/login">Login</Link>
+      <nav className='navbar navbar-expand-md navbar-light d-none d-lg-block sticky-top' role='navigation'>
+        <div className='container-fluid'>
+          <a className='navbar-brand'>When2Poll</a>
+          <Nav className='ml-auto'>
+            <Nav.Item>
+              <Nav.Link to="/" className='nav-link'>
+                Início
+              </Nav.Link>
+            </Nav.Item>
+          </Nav>
         </div>
-        <Outlet />
+      </nav>
+      <nav className='navbar fixed-bottom navbar-light d-block d-lg-none bottom-tab-nav' role='navigation'>
+        <Nav className='w-100'>
+          <div className='d-flex flex-row justify-content-around w-100'>
+            {
+              tabs.map((tab, index) => (
+                <Nav.Item key={`tab-${index}`}>
+                  <Nav.Link to={tab.route} className='nav-link bottom-nav-link' activeClassname="active">
+                    <div className='row d-flex flex-column justify-content-center align-items-center'>
+                      <FontAwesomeIcon size='lg' icon={tab.icon} />
+                      <div className='bottom-tab-label'>{tab.label }</div>
+                    </div>
+                  </Nav.Link>
+                </Nav.Item>
+              ))
+            }
+          </div>
+        </Nav>
+      </nav>
+      <Outlet />
     </>
     
   )
