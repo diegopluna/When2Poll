82a28d500464c377aba86ba8cc4f554527913921
diegopluna <dpl@cesar.school>
2023-04-14 16:03:19 -0300

    Applied beanstalk sqlite fix

--- when2poll/when2poll/settings.py
+++ when2poll/when2poll/settings.py
@@ -138,7 +138,7 @@ WSGI_APPLICATION = 'when2poll.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'utils.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
