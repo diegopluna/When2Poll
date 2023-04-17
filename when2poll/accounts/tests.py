584b9634cec6bfd2054c11000aa4089eb71af44c
diegopluna <dpl@cesar.school>
2023-04-17 19:27:08 -0300

    Added unit test for User and Groups models

--- when2poll/accounts/tests.py
+++ when2poll/accounts/tests.py
@@ -1,3 +1,49 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+
+User = get_user_model()
+
+
+
+class UserModelTest(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            email='test@example.com',
+            full_name='Test User',
+            password='password123'
+        )
+
+    def test_create_user(self):
+        self.assertTrue(isinstance(self.user, get_user_model()))
+        self.assertEqual(self.user.email, 'test@example.com')
+        self.assertEqual(self.user.full_name, 'Test User')
+        self.assertTrue(self.user.is_active)
+        self.assertFalse(self.user.is_staff)
+        self.assertFalse(self.user.is_admin)
+
+    def test_create_staffuser(self):
+        staff_user = get_user_model().objects.create_staffuser(
+            email='staff@example.com',
+            full_name='Staff User',
+            password='password123'
+        )
+        self.assertTrue(isinstance(staff_user, get_user_model()))
+        self.assertEqual(staff_user.email, 'staff@example.com')
+        self.assertEqual(staff_user.full_name, 'Staff User')
+        self.assertTrue(staff_user.is_active)
+        self.assertTrue(staff_user.is_staff)
+        self.assertFalse(staff_user.is_admin)
+
+    def test_create_superuser(self):
+        super_user = get_user_model().objects.create_superuser(
+            email='admin@example.com',
+            full_name='Admin User',
+            password='password123'
+        )
+        self.assertTrue(isinstance(super_user, get_user_model()))
+        self.assertEqual(super_user.email, 'admin@example.com')
+        self.assertEqual(super_user.full_name, 'Admin User')
+        self.assertTrue(super_user.is_active)
+        self.assertTrue(super_user.is_staff)
+        self.assertTrue(super_user.is_admin)
--- when2poll/organizations/tests.py
+++ when2poll/organizations/tests.py
@@ -1,3 +1,55 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from .models import Organization, OrgInvitation
 
-# Create your tests here.
+User = get_user_model()
+
+class OrganizationModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.test_user = User.objects.create_user(full_name='test user', email='test@example.com', password='testpass')
+        cls.test_organization = Organization.objects.create(
+            name='Test Organization',
+            description='This is a test organization',
+            owner=cls.test_user
+        )
+
+    def test_organization_name(self):
+        organization = Organization.objects.get(id=self.test_organization.id)
+        expected_name = 'Test Organization'
+        self.assertEqual(organization.name, expected_name)
+
+    def test_organization_owner(self):
+        organization = Organization.objects.get(id=self.test_organization.id)
+        expected_owner = self.test_user
+        self.assertEqual(organization.owner, expected_owner)
+
+class OrgInvitationModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.test_user = User.objects.create_user(full_name='test user', email='test@example.com', password='testpass')
+        cls.test_organization = Organization.objects.create(
+            name='Test Organization',
+            description='This is a test organization',
+            owner=cls.test_user
+        )
+        cls.test_invitation = OrgInvitation.objects.create(
+            organization=cls.test_organization,
+            user=cls.test_user,
+            accepted=False
+        )
+
+    def test_org_invitation_organization(self):
+        invitation = OrgInvitation.objects.get(id=self.test_invitation.id)
+        expected_organization = self.test_organization
+        self.assertEqual(invitation.organization, expected_organization)
+
+    def test_org_invitation_user(self):
+        invitation = OrgInvitation.objects.get(id=self.test_invitation.id)
+        expected_user = self.test_user
+        self.assertEqual(invitation.user, expected_user)
+
+    def test_org_invitation_accepted(self):
+        invitation = OrgInvitation.objects.get(id=self.test_invitation.id)
+        expected_accepted = False
+        self.assertEqual(invitation.accepted, expected_accepted)
