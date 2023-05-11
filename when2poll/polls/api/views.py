2f0f8a6a0604fd00f95e1eca71e0be876451805a
alwolmer <108356950+alwolmer@users.noreply.github.com>
2023-05-11 06:27:07 -0300

    started poll update

--- when2poll/polls/api/views.py
+++ when2poll/polls/api/views.py
@@ -42,3 +42,8 @@ class AvailabilityPollView(APIView):
             serializer = AvailabilityPollSerializer(poll)
             payload = serializer.data
             return JsonResponse(payload, safe=False)
+    
+    @swagger_auto_schema(request_body=serializer_class, responses={201: serializer_class})
+    def put(self, request):
+        poll_id = request.query_params.get('poll_id')
+        
