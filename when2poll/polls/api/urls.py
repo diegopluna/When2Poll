dd9310002d829a3c026a7521f1e97cd9e8998ec9
alwolmer <108356950+alwolmer@users.noreply.github.com>
2023-05-11 06:16:20 -0300

    started poll update

--- when2poll/polls/api/urls.py
+++ when2poll/polls/api/urls.py
@@ -4,4 +4,5 @@ from .views import AvailabilityPollView
 urlpatterns = [
     path('post/', AvailabilityPollView.as_view(),name="post-poll"),
     path('get/', AvailabilityPollView.as_view(), name="get-polls"),
+    path('put/', AvailabilityPollView.as_view(), name="put-poll"),
 ]
