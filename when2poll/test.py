7f1700d56324a7abd4f9a75b8fd41b5661ebc955
Diego Peter <dpl@cesar.school>
2023-06-08 10:35:04 -0300

    Another test

--- when2poll/test.py
+++ when2poll/test.py
@@ -9,21 +9,21 @@ from time import sleep
 # Exemplos: https://ordinarycoders.com/blog/article/testing-django-selenium
 
 
-# chrome_options = webdriver.ChromeOptions()
-# chrome_options.add_argument("--no-sandbox")
-# chrome_options.add_argument("--headless")
-# chrome_options.add_argument("--disable-gpu")
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-gpu")
 # browser = webdriver.Chrome(options=chrome_options)
 
 # Create your tests here.
 
 class TestHome(LiveServerTestCase):
-
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
         cls.selenium = webdriver.Chrome()
         cls.selenium.implicitly_wait(10)
+        
 
     @classmethod
     def tearDownClass(cls):
@@ -39,4 +39,4 @@ class TestHome(LiveServerTestCase):
 
         self.selenium.get('http://127.0.0.1:8000/')
         sleep(4)
-        self.assertEqual(1,1)
+        assert "When2Poll" in self.selenium.title
