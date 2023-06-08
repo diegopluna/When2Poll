e605a3e1cef873aae81f6d7b9eeb5edcb117bafa
Diego Peter <dpl@cesar.school>
2023-06-08 00:50:03 -0300

    Fixed selenium testing for user creation

--- when2poll/accounts/tests.py
+++ when2poll/accounts/tests.py
@@ -9,7 +9,7 @@ chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-gpu")
-browser = webdriver.Chrome(options=chrome_options)
+browser = webdriver.Chrome(options=chrome_options,executable_path=r'.\chromedriver.exe')
 
 
 # Create your tests here.
