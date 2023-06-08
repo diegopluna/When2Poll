eceb36d8e4c7d0c5deef7ef151ffc4c4c4106cca
Diego Peter <dpl@cesar.school>
2023-06-08 18:20:49 -0300

    Test fix

--- tests.py
+++ tests.py
@@ -9,9 +9,9 @@ import time
 
 
 chrome_options = webdriver.ChromeOptions()
-# chrome_options.add_argument("--no-sandbox")
-# chrome_options.add_argument("--headless")
-# chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-gpu")
 driver = webdriver.Chrome(options=chrome_options)
 
 # Create your tests here.
