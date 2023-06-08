cf1ab148838508e55c3a0ea7c37ff4f6463c2a36
Diego Peter <dpl@cesar.school>
2023-06-08 12:29:12 -0300

    Testing again

--- accounts/tests.py
+++ accounts/tests.py
@@ -5,11 +5,11 @@ from selenium.webdriver.common.by import By
 # Documentação selenium para Python: https://selenium-python.readthedocs.io/
 # Exemplos: https://ordinarycoders.com/blog/article/testing-django-selenium
 
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-gpu")
-browser = webdriver.Chrome(options=chrome_options)
+# chrome_options = webdriver.ChromeOptions()
+# chrome_options.add_argument("--no-sandbox")
+# chrome_options.add_argument("--headless")
+# chrome_options.add_argument("--disable-gpu")
+# browser = webdriver.Chrome(options=chrome_options)
 
 
 # Create your tests here.
