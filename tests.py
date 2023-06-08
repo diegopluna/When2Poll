a5e63af1a093f12ff12b3de9cc93539b9e7daae6
diegopluna <dpl@cesar.school>
2023-06-08 16:58:27 -0300

    Updating tests

--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import requests
 
 # Documentação selenium para Python: https://selenium-python.readthedocs.io/
 # Exemplos: https://ordinarycoders.com/blog/article/testing-django-selenium
@@ -15,13 +16,13 @@ chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-gpu")
+print(requests.__version__)
 browser = webdriver.Chrome(options=chrome_options)
 
 # Create your tests here.
 
 class TestHome(LiveServerTestCase):
     def test_title(self):
-        
         browser.get('http://127.0.0.1:8000/')
         assert "When2Poll" in browser.title
 
