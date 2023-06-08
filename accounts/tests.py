ede0fade2b5cdee8f41bfc9de663d7e678ccb12e
Diego Peter <dpl@cesar.school>
2023-06-08 11:24:07 -0300

    Testing again

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,5 +1,6 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from time import sleep
@@ -8,23 +9,19 @@ from time import sleep
 # Documentação selenium para Python: https://selenium-python.readthedocs.io/
 # Exemplos: https://ordinarycoders.com/blog/article/testing-django-selenium
 
-
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-gpu")
-# browser = webdriver.Chrome(options=chrome_options)
-
 # Create your tests here.
 
 class TestHome(LiveServerTestCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
+        chrome_options = Options()
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--disable-gpu")
         cls.selenium = webdriver.Chrome(options=chrome_options)
         cls.selenium.implicitly_wait(10)
-        
-
+    
     @classmethod
     def tearDownClass(cls):
         cls.selenium.quit()
@@ -37,6 +34,6 @@ class TestHome(LiveServerTestCase):
         # sleep(2)
         # assert "When2Poll" in browser.title
 
-        self.selenium.get('http://127.0.0.1:8000/')
+        self.selenium.get(self.live_server_url)
         sleep(4)
         assert "When2Poll" in self.selenium.title
