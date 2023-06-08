3359919028f6312166141547a00f3a4842971daa
Diego Peter <dpl@cesar.school>
2023-06-08 00:59:39 -0300

    Fixed selenium testing for user creation

--- when2poll/accounts/tests.py
+++ when2poll/accounts/tests.py
@@ -1,15 +1,23 @@
 from django.test import LiveServerTestCase
-from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+
 
 # Documentação selenium para Python: https://selenium-python.readthedocs.io/
 # Exemplos: https://ordinarycoders.com/blog/article/testing-django-selenium
 
-browser = WebDriver()
+
 
 
 # Create your tests here.
 
 class TestHome(LiveServerTestCase):
     def test_title(self):
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--disable-gpu")
+        browser = webdriver.Chrome(options=chrome_options)
         browser.get('http://127.0.0.1:8000/')
         assert "When2Poll" in browser.title
