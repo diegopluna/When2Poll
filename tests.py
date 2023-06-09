b90bde1a727f64e906eaf4a9d70ebf0fdfc40414
João Pedro Batista <jpbs@cesar.school>
2023-06-09 17:47:28 -0300

    new super-user

--- tests.py
+++ tests.py
@@ -14,11 +14,11 @@ import datetime
 
 
 chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument('window-size=1920,1080')
-chrome_options.add_argument("--start-maximized")
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-gpu")
+#chrome_options.add_argument("--no-sandbox")
+#chrome_options.add_argument('window-size=1920,1080')
+#chrome_options.add_argument("--start-maximized")
+#chrome_options.add_argument("--headless")
+#chrome_options.add_argument("--disable-gpu")
 driver = webdriver.Chrome(options=chrome_options)
 
 # Create your tests here.
