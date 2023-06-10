27f807066b36facdc95c01e5642abe53bf297cdc
João Pedro Batista da silva <116274892+Joaopedrobs7@users.noreply.github.com>
2023-06-10 14:35:44 -0300

    test fix013
    trying to fix

--- tests.py
+++ tests.py
@@ -25,12 +25,12 @@ import datetime
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--headless=new")
+chrome_options.add_argument("--headless")
 chrome_options.add_argument("window-size=1080,1440")
-chrome_options.add_argument("--start-maximized")
+#chrome_options.add_argument("--start-maximized")
 chrome_options.add_argument("--disable-gpu")
-chrome_options.add_argument("force-device-scale-factor=0,5")
-#chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument("force-device-scale-factor=0.25")
+chrome_options.add_argument('--disable-dev-shm-usage')
 #chrome_options.add_argument("--disable-extensions")
 #chrome_options.add_argument("--start-fullscreen")
 
