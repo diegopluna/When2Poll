f94601dab3784d470596dfd1b23ed071685602d4
diegopluna <dpl@cesar.school>
2023-06-09 21:23:07 -0300

    Reestructuring for release

--- when2poll/settings.py
+++ when2poll/settings.py
@@ -180,10 +180,10 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'assets/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'reactapp/build/static')
+    os.path.join(BASE_DIR, 'reactapp/dist/assets/')
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
