db38d04a2da7ae875d15be5e7288399c66391a20
Diego Peter <dpl@cesar.school>
2023-06-12 18:22:09 -0300

    Updating release settings and requirements

--- when2poll/settings.py
+++ when2poll/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-$blsifwuajbmhqzy2b-bl4+1-(^s@=qhk)96(*&fp#*9$&*sdo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["when2poll-env.eba-cpqbamxu.sa-east-1.elasticbeanstalk.com"]
+ALLOWED_HOSTS = ["when2poll-env.eba-cpqbamxu.sa-east-1.elasticbeanstalk.com", "localhost"]
 
 
 # Application definition
