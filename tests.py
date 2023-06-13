82c18ee68f9691b924f899c2685eaac635da01ed
João Pedro Batista da silva <116274892+Joaopedrobs7@users.noreply.github.com>
2023-06-12 22:24:31 -0300

    add headless

--- tests.py
+++ tests.py
@@ -25,9 +25,9 @@ import datetime
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--disable-browser-side-navigation")
-#chrome_options.add_argument("--no-sandbox")
-#chrome_options.add_argument("--headless")
-chrome_options.add_argument("window-size=1920,1080")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("window-size=1440,1080")
 #chrome_options.add_argument("--start-maximized")
 #chrome_options.add_argument("--disable-gpu")
 #chrome_options.add_argument("force-device-scale-factor=0.5")
